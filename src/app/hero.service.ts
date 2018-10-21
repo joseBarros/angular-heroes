04853d3cf0c124f9288337022f618eb67dc74d9e
joseBarros <josebarros.1987@gmail.com>
2018-10-21 15:59:06 +0100

    You extended HeroService to support post(), put(), and delete() methods. You updated the components to allow adding, editing, and deleting of heroes.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,64 +3,124 @@ import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 import {Observable, of} from 'rxjs';
 import {MessageService} from './message.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, map, tap } from 'rxjs/operators';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {catchError, map, tap} from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 // You registered the HeroService as the provider of its service at the root level so that it can be injected anywhere in the app.
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class HeroService {
 
-    private heroesUrl = 'api/heroes';  // URL to web api
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-    // Angular will inject the singleton MessageService into that property when it creates the HeroService.
-    // Typical "service-in-service" scenario: you inject the MessageService into the HeroService which is injected into the HeroesComponent.
-    // Inject HttpClient into the constructor in a private property called http.
-    constructor(
-        private http: HttpClient,
-        private messageService: MessageService) { }
+  // Angular will inject the singleton MessageService into that property when it creates the HeroService.
+  // Typical "service-in-service" scenario: you inject the MessageService into the HeroService which is injected into the HeroesComponent.
+  // Inject HttpClient into the constructor in a private property called http.
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) {
+  }
 
-    // You gave the HeroService get data method an asynchronous signature.
-    // You used RxJS of() to return an observable of mock heroes (Observable<Hero[]>).
-    // HttpClient.get returns untyped JSON object by default. Applying the optional type specifier, <Hero[]> , returns an object.
-    /** GET heroes from the server */
-    getHeroes (): Observable<Hero[]> {
-        return this.http.get<Hero[]>(this.heroesUrl)
-            .pipe(
-                catchError(this.handleError('getHeroes', []))
-            );
+  // You gave the HeroService get data method an asynchronous signature.
+  // You used RxJS of() to return an observable of mock heroes (Observable<Hero[]>).
+  // HttpClient.get returns untyped JSON object by default. Applying the optional type specifier, <Hero[]> , returns an object.
+  // The catchError() operator intercepts an Observable that failed. It passes the error an error handler that can do what it wants with the error.
+  //The following handleError() method reports the error and then returns an innocuous result so that the application keeps working.
+  //The HeroService methods will tap into the flow of observable values and send a message (via log()) with the RxJS tap operator, which looks at the observable values, does something with those values, and passes them along.
+  // The tap call back doesn't touch the values themselves.
+  /** GET heroes from the server */
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError('getHeroes', []))
+      );
+  }
+
+  // Like getHeroes(), getHero() has an asynchronous signature. It returns a mock hero as an Observable, using the RxJS of() function.
+  // returns an Observable<Hero> ("an observable of Hero objects")
+  /** GET hero by id. Will 404 if id not found */
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  // It returns an error handler function to catchError that it has configured with both the name of the operation that failed and a safe return value
+  // Because each service method returns a different kind of Observable result, handleError() takes a type parameter so it can return the safe value as the type that the app expects.
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  //uses http.put() to persist the changed hero on the server.
+  /** PUT: update the hero on the server */
+  updateHero (hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  //it expects the server to generates an id for the new hero, which it returns in the Observable<Hero> to the caller.
+  /** POST: add a new hero to the server */
+  addHero (hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  //you don't send data as you did with put and post.
+  // you still send the httpOptions.
+  /** DELETE: delete the hero from the server */
+  deleteHero (hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  // The method returns immediately with an empty array if there is no search term. Resembles getHeroes(.
+  // The only significant difference is the URL, which includes a query string with the search term.
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
     }
-
-    // Like getHeroes(), getHero() has an asynchronous signature. It returns a mock hero as an Observable, using the RxJS of() function.
-    getHero(id: number): Observable<Hero> {
-        // TODO: send the message _after_ fetching the hero
-        this.messageService.add(`HeroService: fetched hero id=${id}`);
-        return of(HEROES.find(hero => hero.id === id));
-    }
-
-    /** Log a HeroService message with the MessageService */
-    private log(message: string) {
-        this.messageService.add(`HeroService: ${message}`);
-    }
-
-    /**
-     * Handle Http operation that failed.
-     * Let the app continue.
-     * @param operation - name of the operation that failed
-     * @param result - optional value to return as the observable result
-     */
-    private handleError<T> (operation = 'operation', result?: T) {
-        return (error: any): Observable<T> => {
-
-            // TODO: send the error to remote logging infrastructure
-            console.error(error); // log to console instead
-
-            // TODO: better job of transforming error for user consumption
-            this.log(`${operation} failed: ${error.message}`);
-
-            // Let the app keep running by returning an empty result.
-            return of(result as T);
-        };
-    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,6 +19,26 @@ export class HeroesComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes);
   }
 
+  //When the given name is non-blank, the handler creates a Hero-like object from the name (it's only missing the id) and passes it to the services addHero() method.
+  //When addHero saves successfully, the subscribe callback receives the new hero and pushes it into to the heroes list for display.
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  // Although the component delegates hero deletion to the HeroService, it remains responsible for updating its own list of heroes.
+  // The component's delete() method immediately removes the hero-to-delete from that list, anticipating that the HeroService will succeed on the server.
+  // There's really nothing for the component to do with the Observable returned by heroService.delete(). It must subscribe anyway.
+  // If you neglect to subscribe(), the service will not send the delete request to the server! As a rule, an Observable does nothing until something subscribes!
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
   // Reserve the constructor for simple initialization such as wiring constructor parameters to properties.
   // While you could call getHeroes() in the constructor, that's not the best practice.
   // Angular will inject the singleton HeroService into that property when it creates the HeroesComponent.
